8cfd5a03035166c3af049fcd37c365114e387b6d
TURKCELL\TCASENYURT <alper.senyurt@turkcell.com.tr>
2020-03-11 13:20:00 +0300

    Initialize Project

---
+++ ForeksCase/ForeksCase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForeksCase
+//
+//  Created by yasin on 11.03.2020.
+//  Copyright © 2020 yt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
